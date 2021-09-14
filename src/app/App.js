533a8cb38aef0d4107c146a363aac430af69d4a0
MAbdurahman <mdbdrrhm@gmail.com>
2021-09-13 20:40:32 -0500

    Update project with css and jsx

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from 'react'
+import data from './../data'
+import List from './../components/List';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+    const [people, setPeople] = useState(data)
+    return (
+        <main>
+            <section className='container'>
+                <h3>{people.length} birthdays today</h3>
+                <List people={people} />
+                <button onClick={() => setPeople([])}>clear all</button>
+            </section>
+        </main>
+    )
 }
 
-export default App;
+export default App
---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+    {
+        id: 1,
+        name: 'Bertie Yates',
+        age: 29,
+        image:
+            'https://res.cloudinary.com/mdbdrrhm/image/upload/v1631578673/people/person-2_drxbil.jpg',
+    },
+    {
+        id: 2,
+        name: 'Hester Hogan',
+        age: 32,
+        image:
+            'https://res.cloudinary.com/mdbdrrhm/image/upload/v1631579493/people/person-19_iukl41.jpg',
+    },
+    {
+        id: 3,
+        name: 'Larry Little',
+        age: 36,
+        image:
+            'https://res.cloudinary.com/mdbdrrhm/image/upload/v1631579325/people/person-12_ciyd4x.jpg',
+    },
+    {
+        id: 4,
+        name: 'Joan Walsh',
+        age: 34,
+        image:
+            'https://res.cloudinary.com/mdbdrrhm/image/upload/v1631578748/people/person-11_zq8brv.jpg',
+    },
+    {
+        id: 5,
+        name: 'Lola Gardner',
+        age: 29,
+        image:
+            'https://res.cloudinary.com/mdbdrrhm/image/upload/v1631578693/people/person-3_ofgt08.jpg',
+    },
+];
